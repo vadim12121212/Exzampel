130ef2a80eaec74a6100eec31e4ff64538e4c90b
*vadim12121212* <imailcom8@gmail.com>
2023-02-10 12:39:24 +0300

    Example 005 if

---
+++ Example005_if/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Вводи имя: ");
+String usrename = Console.ReadLine();
+if (usrename.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же Маша");
+} 
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (usrename);    
+}
