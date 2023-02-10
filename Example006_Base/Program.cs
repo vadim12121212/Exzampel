f56ab1185fb6335dbbd6b4ffe9fa555a2f2c2579
*vadim12121212* <imailcom8@gmail.com>
2023-02-10 13:12:41 +0300

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberA = 1;
+int numberB = 8;
+int numberC = 3;
+int numberD = 3;
+int numberE = 6;
+int max = numberA;
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+if (numberD > max) max = numberD;
+if (numberE > max) max = numberE;
+Console.Write("max = ");
+Console.WriteLine (max);
+
